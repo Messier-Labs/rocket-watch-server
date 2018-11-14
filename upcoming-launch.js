03e76fb4218dec99fc693146fa9d15b6de7db919
Sivasubramanyam A <ssdragonar5@gmail.com>
2018-11-14 21:59:28 +0530

    Add PSLV's November 26th launch

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,5 +1,6 @@
 module.exports = {
-  date: '2018-11-14T11:38:00.587Z',
-  launchPad: 2,
-  name: 'GSLV Mk III-D2 / GSAT-29'
+  date: '2018-11-25T18:30:00.346Z',
+  launchPad: 1,
+  name: 'PSLV / HySIS'
 }
+

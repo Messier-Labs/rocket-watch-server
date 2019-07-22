2812b9d7692097a32cab444d85f5143bf2081fd3
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-07-22 07:55:38 +0530

    Update upcoming-launch.js

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // time in GST YYYY-MM-DD
-  date: '2019-07-14T21:21:00.000Z',
+  // time in GMT YYYY-MM-DD
+  date: '2019-07-22T09:13:00.000Z',
   launchPad: 2,
   name: 'GSLV/MkIII - Chandrayaan 2'
 }

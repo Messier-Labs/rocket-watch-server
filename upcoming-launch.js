3b8abb8869e118864bff86f3b84f2f82be92ff18
Sivasubramanyam A <ssdragonar5@gmail.com>
2020-11-06 13:01:03 +0530

    Update upcoming-launch.js

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,6 +1,6 @@
 module.exports = {
   // time in GMT YYYY-MM-DD
-  date: '2019-12-11T09:55:00.000Z',
+  date: '2020-11-07T09:32:00.000Z',
   launchPad: 1,
-  name: 'PSLV-C48 - RISAT-2BR1'
+  name: 'PSLV-C49/EOS-01'
 }

a8dbe0d4a38604b186b307d8b9e5535b73f145e2
Sivasubramanyam A <ssdragonar5@gmail.com>
2020-12-17 12:54:41 +0530

    Update upcoming-launch.js

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,6 +1,6 @@
 module.exports = {
   // time in GMT YYYY-MM-DD
-  date: '2020-11-07T09:32:00.000Z',
-  launchPad: 1,
-  name: 'PSLV-C49/EOS-01'
+  date: '2020-12-17T10:11:00.000Z',
+  launchPad: 2,
+  name: 'PSLV-C50/CMS-01'
 }

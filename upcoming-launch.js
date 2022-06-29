4efd29f86516ba53c8cfe59837e90670af0ac499
Sivasubramanyam A <ssdragonar5@gmail.com>
2022-06-29 21:18:31 +0530

    Update upcoming-launch.js

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,6 +1,6 @@
 module.exports = {
   // time in GMT YYYY-MM-DD
-  date: '2022-02-14T00:29:00.000Z',
+  date: '2022-06-29T12:32:00.000Z',
   launchPad: 1,
-  name: 'PSLV-C52/RISAT-1A'
+  name: 'PSLV-C53/DS-EO'
 }

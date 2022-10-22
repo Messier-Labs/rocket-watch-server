15a429374f10b95103bd10cf5fd0fbaf106c43c1
Sivasubramanyam A <ssdragonar5@gmail.com>
2022-10-22 15:10:12 +0530

    Update upcoming-launch.js

--- upcoming-launch.js
+++ upcoming-launch.js
@@ -1,6 +1,6 @@
 module.exports = {
   // time in GMT YYYY-MM-DD
-  date: '2022-06-30T12:32:00.000Z',
+  date: '2022-12-22T18:37:00.000Z',
   launchPad: 2,
-  name: 'PSLV-C53/DS-EO'
+  name: 'LVM3-M2'
 }
